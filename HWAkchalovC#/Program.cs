3f11b599697e45810f17c316644741a95710ab14
Anvar <akchalov.a.m@yandex.ru>
2022-11-30 20:59:54 +0500

    Task 2 added

--- HWAkchalovC#/Program.cs
+++ HWAkchalovC#/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
+/*
 Console.Write("Input a first number:");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number:");
@@ -12,4 +13,32 @@ if(num1 > num2)
 else
 {
     Console.WriteLine(num2);
+}
+*/
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Inter some number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Inter some number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Inter some number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int MaxNum = 0;
+
+if(num1 > num2)
+{
+    MaxNum = num1;
+}
+else
+{
+    MaxNum = num2;
+}
+if(MaxNum > num3)
+{
+    Console.WriteLine("Maximum number: " + MaxNum);
+}
+else
+{
+    Console.WriteLine("Maximum number: " + num3);
 }
