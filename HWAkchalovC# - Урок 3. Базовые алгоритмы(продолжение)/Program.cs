ce16635d79f0a15bc79842e1008af8ab57ce7580
Anvar <akchalov.a.m@yandex.ru>
2022-12-09 23:43:57 +0500

    HW Task 19, 21.

--- HWAkchalovC# - Урок 3. Базовые алгоритмы(продолжение)/Program.cs
+++ HWAkchalovC# - Урок 3. Базовые алгоритмы(продолжение)/Program.cs
@@ -1,7 +1,9 @@
 ﻿// HOMEWORK FROM 08.12.2022
+
 //Задача 19.
 //Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-
+// Решил для всех чисел. 
+/*
 int Inverted(int somNum)
 { 
     int num = somNum;// заданое число - делим на 10 до 0
@@ -32,3 +34,30 @@ int invNum = Inverted(randNum);
 bool result = Check(randNum,invNum);
 
 Console.WriteLine($"Some number is {randNum}. Inverted number is {invNum}. Task response: {result}.");
+*/
+
+//Задача 21.
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+/*
+double Distance(double xA, double yA, double zA, double xB, double yB, double zB)
+{
+    double resalt = Math.Sqrt(Math.Pow((xA - xB),2) + Math.Pow((yA - yB),2) + Math.Pow((zA - zB),2));
+    return Math.Round(resalt, 1);
+}
+
+Console.Write("Input xA: ");
+double xApoint = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input yA: ");
+double yApoint = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input zA: ");
+double zApoint = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input xB: ");
+double xBpoint = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input yB: ");
+double yBpoint = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input zB: ");
+double zBpoint = Convert.ToDouble(Console.ReadLine());
+
+double resaltDistsnce = Distance(xApoint, yApoint, zApoint, xBpoint, yBpoint, zBpoint);
+Console.WriteLine($"Distanse bitween points A({xApoint};{yApoint};{zApoint}) and B({xBpoint};{yBpoint};{zBpoint}) is equal to {resaltDistsnce}.");
+*/
